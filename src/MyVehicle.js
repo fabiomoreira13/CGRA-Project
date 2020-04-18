0833360d05f8f09a363818e8ee46854449111218
Fabio Moreira <moreira.fbio8@gmail.com>
2020-04-18 15:51:10 +0100

    Finished blimp design. Must choose proper textures!

--- src/MyVehicle.js
+++ src/MyVehicle.js
@@ -166,6 +166,30 @@ class MyVehicle extends CGFobject {
 		this.leme.display();
 		this.scene.popMatrix();
 
+
+
+		
+		this.scene.pushMatrix();
+		this.scene.translate(-0.3, 0, -2.1);
+		
+		this.scene.rotate(Math.PI, 0,0,1);
+		this.scene.rotate(Math.PI /2, 1,0,0);
+		
+		this.scene.scale(0.4,0.4,0.4);
+		this.leme.display();
+		this.scene.popMatrix();
+
+
+
+		this.scene.pushMatrix();
+		this.scene.translate(0.3, 0, -2.1);
+		
+		this.scene.rotate(Math.PI, 0,0,1);
+		this.scene.rotate(Math.PI /2, 1,0,0);
+		
+		this.scene.scale(0.4,0.4,0.4);
+		this.leme.display();
+		this.scene.popMatrix();
 		
 	}
 
