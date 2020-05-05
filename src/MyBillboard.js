5169569c63c2bb3a51f2a7113188d6c40b02ac78
Fabio Moreira <moreira.fbio8@gmail.com>
2020-05-05 16:40:17 +0100

    Updated plane file. Going to try and add the progress bar!

--- src/MyBillboard.js
+++ src/MyBillboard.js
@@ -26,6 +26,8 @@ class MyBillboard extends CGFobject{
         this.message.setTexture(this.texture);
         this.message.setTextureWrap('REPEAT', 'REPEAT');
 
+
+        
 		
 		
 	}
@@ -33,13 +35,24 @@ class MyBillboard extends CGFobject{
 
 	display(){
 		this.scene.pushMatrix();
-
-        
-        
-        this.scene.scale(2,1,1);
+        this.scene.scale(1,0.5,1);
         this.message.apply();
         this.rectangle.display();
-		this.scene.popMatrix();
+        this.scene.popMatrix();
+        
+        this.scene.pushMatrix();
+        this.scene.translate(-0.95,-1,0);
+        this.scene.scale(0.05,0.5,1);
+        this.rectangle.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+        this.scene.translate(0.95,-1,0);
+        this.scene.scale(0.05,0.5,1);
+        this.rectangle.display();
+        this.scene.popMatrix();
+
+        
 		
 
 	}
